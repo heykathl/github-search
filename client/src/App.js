043b344af541908c4b552aea602271d1366ea5ad
Kathleen Ly <heykathl@users.noreply.github.com>
2022-05-28 10:25:57 +0200

    Included axios to get request from api, made function to respond to onclick search button

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import axios from 'axios'
+import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
+
+
+
+  const handleApiSearch = async () => {
+    await axios.get(`https://api.github.com/users/heykathl/repos`)
+    .then((response) => {
+      console.log(response.data[0].language)
+    
+    })
+  }
+
   return (
     <>
     <h1>GitHub Search</h1>
+    <input type="text"/>
+    <button onClick={handleApiSearch}>Search</button>
     </>
   );
 }
